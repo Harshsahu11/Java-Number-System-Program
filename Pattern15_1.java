489a2ee7dd882db13a657887dac3965a99651e99
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2024-09-23 21:58:08 +0530

    Add files via upload

---
+++ Pattern15_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Pattern15_1 {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        int sp=n/2;
+        int st=1;
+        for(int i=1;i<=n;i++)
+        {
+        //  System.out.print(sp+" , "+st);
+          for(int j=1;j<=sp;j++){
+              System.out.print("\t");
+          }
+          for(int k=1;k<=st;k++){
+              System.out.print("1"+"\t");
+          }
+            if(i<=n/2){
+                sp--;
+                st+=2;
+            }
+            else{
+                sp++;
+                st-=2;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern15_3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pattern15_3 {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int n=Sc.nextInt();
+        int sp=n/2;
+        int st=1;
+        int temp;
+        for(int i=1;i<=n;i++)
+        {
+            //  System.out.print(sp+" , "+st);
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int k=1;k<=st;k++){
+                System.out.print(i+"\t");
+            }
+            if(i<=n/2){
+                sp--;
+                st+=2;
+            }
+            else{
+                sp++;
+                st-=2;
+            }
+            System.out.println();
+        }
+    }
+}
